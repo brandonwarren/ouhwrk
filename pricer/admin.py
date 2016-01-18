4248989f2e10727653c6f67e2a9eb733a5786b05
Brandon Warren <brandon@americantorque.com>
2016-01-18 00:55:40 +0000

    Configure for access to database on AWS. BUG: seems to be unaware of fields except for id.

--- pricer/admin.py
+++ pricer/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from models import ItemSale
 
-# Register your models here.
+admin.site.register(ItemSale)
+
--- pricer/models.py
+++ pricer/models.py
@@ -1,5 +1,26 @@
 from __future__ import unicode_literals
 
+from django.utils.encoding import python_2_unicode_compatible
 from django.db import models
 
-# Create your models here.
+# used the following link to verify my model (told me about managed and max_length size):
+# https://docs.djangoproject.com/en/1.9/howto/legacy-databases/
+# can try later:
+#   * Make sure each model has one field with primary_key=True
+# tried without declaring "id" field, then declared it. same problem with both: cannot see fields other than id.
+
+@python_2_unicode_compatible
+class ItemSale(models.Model):
+    id = models.IntegerField(primary_key=True) # id (int): unique id for an item, created by Postgres
+    title = models.CharField(max_length=120, help_text='Title of the item (e.g. Xbox One)'),
+    list_price = models.IntegerField(help_text='The price at which the item was listed'),
+    sell_price = models.IntegerField(help_text='The price at which the item was sold'),
+    city = models.CharField(max_length=120, help_text='The city in which the item was listed'),
+    cashless = models.BooleanField(help_text='The seller will accept a credit card payment'),
+
+    class Meta:
+        managed = False
+        db_table = 'itemPrices_itemsale'
+
+    def __str__(self):
+        return '%d %s' % (self.id, self.title)
