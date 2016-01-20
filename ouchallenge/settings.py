23f1ff29b6b9d2eff5a957a0a6b9620ed66e66aa
Brandon Warren <brandon@americantorque.com>
2016-01-20 06:01:39 +0000

    DEBUG=F. Made faster(?) version of item-price-service, previous version is item-price-service2.

--- ouchallenge/settings.py
+++ ouchallenge/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '43b@w(gh^p-ycc#qwsfwfsvz!wut211o-5^j^9(m4+h82f_pj4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', ]
 
 
 # Application definition
--- pricer/views.py
+++ pricer/views.py
@@ -1,11 +1,49 @@
-import operator
-
-from django.shortcuts import render
 from django.http import JsonResponse # Django 1.7+
-
+from django.db.models import Count
 from pricer.models import ItemSale, ItemSaleLH
 
 def item_price(request):
+    def ret_404():
+        # could just create str_404, but doing so uses CPU for non-404 cases
+        return JsonResponse({
+            'status': 404,
+            'content': { 'message': 'Not found' }})
+
+    item = request.GET.get('item', '')
+    city = request.GET.get('city', '')
+    if not item:
+        return ret_404()
+    list_prices = ItemSaleLH.objects.filter(title=item)
+    if city:
+        list_prices = list_prices.filter(city=city)
+    else:
+        city = 'Not specified'
+    total_count = list_prices.count()
+    if not total_count:
+        return ret_404()
+    list_prices = list_prices.values('list_price').annotate(num_items=Count('list_price')).order_by('-num_items')
+    price = list_prices[0]['list_price']
+    count = list_prices[0]['num_items']
+    # see if there are any ties
+    for price_info in list_prices[1:]:
+        if count != price_info['num_items']:
+            break
+        if price_info['list_price'] > price:
+            # a popularity tie (same count), so keep the highest list price value
+            price = price_info['list_price']
+
+    content = {
+        "item": item,
+        "item_count": total_count, # example results are closer to count, but spec says total_count
+        "price_suggestion": price,
+        "city": city
+    }
+    return JsonResponse({'status': 200,
+                         'content': content})
+
+# original version, straight-forward approach
+import operator
+def item_price2(request):
     def ret_404():
         # could just create str_404, but doing so uses CPU for non-404 cases
         return JsonResponse({
@@ -26,17 +64,12 @@ def item_price(request):
         return ret_404()
     # calculate the mode of list_prices
     price_counts = {}
-    #print 'BEGIN list_prices ------'
     for list_price in list_prices:
-        # print list_price,
         if list_price in price_counts:
             price_counts[list_price] += 1
         else:
             price_counts[list_price] = 1
-    #print 'END------BEGIN sorted_list_prices'
     sorted_list_prices = sorted(list_prices)
-    #print sorted_list_prices
-    #print '----- END -----'
     # suboptimal because we don't need to sort items less popular than the most popular one
     # find max, and all the values with same max value?
     sorted_price_counts = sorted(price_counts.items(), key=operator.itemgetter(1), reverse=True)
